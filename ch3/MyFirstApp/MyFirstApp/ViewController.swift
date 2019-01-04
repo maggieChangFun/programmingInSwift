105382dc11f8702284365d4b16b8d1889f3d338b
maggieChangFun <maggiechang.w@gamil.com>
2019-01-04 16:15:11 +0800

    ch2,ch3

---
+++ ch3/MyFirstApp/MyFirstApp/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by 張芷瑄 on 2019/1/2.
+//  Copyright © 2019 MaggieChang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    //顯示表情符號文字表示
+    @IBAction func showMessage(sender: UIButton) {
+        
+        //建立存放表情符號的字典
+        var emojiDict = ["🤥":"lie","👻":"ghost","🤐":"shut up","👾":"monster"]
+        
+        // sender 是使用者所按下的按鈕
+        // 這裡我們將 sender 儲存至 selectedButton constant
+        let selectedButton = sender
+        
+        if (selectedButton.titleLabel?.text) != nil {
+            
+            // 前端所按的參數
+            let meaning = sender.currentTitle
+            
+            // Change the line below to display the meaning of the emoji instead of Hello World
+            let alertController = UIAlertController(title: "Welcome to My First App", message: emojiDict[meaning!], preferredStyle: UIAlertController.Style.alert)
+            
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            present(alertController, animated: true, completion: nil)
+        }
+    }
+
+}
+
