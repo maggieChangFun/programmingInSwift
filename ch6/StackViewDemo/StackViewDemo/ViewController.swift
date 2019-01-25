e307c125f553f59e633c61a41ce9be89b13696c6
maggie <maggiechang.w@gmail.com>
2019-01-25 17:01:16 +0800

    ch6
    使用堆疊視圖設計UI

---
+++ ch6/StackViewDemo/StackViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackViewDemo
+//
+//  Created by 張芷瑄 on 2019/1/21.
+//  Copyright © 2019 MaggieChang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
